c65968a9050614d4e6ab3178bcc1d6944e6a27c5
Holly Emblem <h.emblem@biocare.co.uk>
2016-06-07 14:24:24 +0100

    Add on-screen tracker code

---
+++ onscreen-tracker.js
@@ -0,0 +1,12 @@
+(function($) {
+    var onScreen = false;
+    $(document).scroll(function(event) {
+        var screenTest = $('.cta').isOnScreen();
+        if (screenTest && !onScreen) {
+            $('.cta').addClass('onScreen');
+            var $callToAction = document.querySelector("a.cta.onScreen").getAttribute("href");
+            onScreen = true;
+            ga('send', 'event', 'CTA', 'Visible', $callToAction);
+        } else {}
+    });
+})(jQuery);
